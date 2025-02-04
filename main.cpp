9fe7e1133e352823a64bffa423f76a23604a288d
khuonghoanghuy <khuonghoanghuy@gmail.com>
2025-02-04 10:52:28 +0700

    added window setted stuff

--- main.cpp
+++ main.cpp
@@ -4,7 +4,14 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode({320, 280}), "SaClicker");
     sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    shape.setFillColor(sf::Color::White);
+
+    // Config Window
+    window.setPosition({100, 100});
+    window.setSize({320, 280});
+    sf::Vector2u size =  window.getSize();
+    auto [width, height] = size;
+    bool focus = window.hasFocus();
 
     while (window.isOpen())
     {
