9a30d9739613082a880cad9cc1ae4476369c17e0
khuonghoanghuy <khuonghoanghuy@gmail.com>
2025-02-04 10:46:42 +0700

    added run.sh, .gitignore, update window size and change title, change name file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <SFML/Graphics.hpp>
 
-int main() {
-    
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({320, 280}), "SaClicker");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 }
---
+++ run.sh
@@ -0,0 +1,3 @@
+g++ -c main.cpp
+g++ main.o -o bin/saClicker-app -lsfml-graphics -lsfml-window -lsfml-system
+./bin/saClicker-app
