936dbde0d57f55b430a339ea436663c9cde734db
khuonghoanghuy <khuonghoanghuy@gmail.com>
2025-02-04 11:04:58 +0700

    add font, text, function overlaps thingie

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include <SFML/Graphics.hpp>
 
+int score = 0;
+
 int main()
 {
+    // Make Circle
     sf::RenderWindow window(sf::VideoMode({320, 280}), "SaClicker");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::White);
+
+    // Make Text
+    sf::Font font;
+    if (!font.loadFromFile("fonts/vcr.ttf")) {
+        // error should popup
+    }
+    sf::Text text;
+    text.setFont(font);
+    text.setString("Click: " + std::to_string(score));
+    text.setCharacterSize(16);
 
     // Config Window
     window.setPosition({100, 100});
@@ -20,10 +33,20 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            else if (event.type == sf::Event::MouseButtonPressed) {
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+                    if (shape.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
+                        score += 1;
+                        text.setString("Score: " + std::to_string(score));
+                    }
+                }
+            }
         }
 
         window.clear();
         window.draw(shape);
+        window.draw(text);
         window.display();
     }
 }
